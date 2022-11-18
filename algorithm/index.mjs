146a2a01192d2abef2c96a7cb11538f09437975d
Angel Meneses <angelmenesest123@gmail.com>
2022-11-17 23:21:35 -0500

    feat: initial impl of our algorithm

--- algorithm/index.mjs
+++ algorithm/index.mjs
@@ -1,17 +1,280 @@
 import inquirer from "inquirer";
 
+const currencyFormatter = new Intl.NumberFormat("es-PE", { style: "currency", currency: "PEN" });
+const percentageFormatter = new Intl.NumberFormat("es-PE", { style: "percent" });
+
+function roundMoney(num) {
+  return Math.round((num + Number.EPSILON) * 100) / 100;
+}
+
+function inDays(frequency) {
+  switch (frequency) {
+    case "Diaria":
+      frequency = 1;
+      break;
+    case "Quincenal":
+      frequency = 15;
+      break;
+    case "Mensual":
+      frequency = 30;
+      break;
+    case "Bimestral":
+      frequency = 60;
+      break;
+    case "Trimestral":
+      frequency = 90;
+      break;
+    case "Cuatrimestral":
+      frequency = 120;
+      break;
+    case "Semestral":
+      frequency = 180;
+      break;
+    case "Anual":
+      frequency = 360;
+      break;
+  }
+  return frequency;
+}
+
+const frequencies = [
+  "Diaria",
+  "Quincenal",
+  "Mensual",
+  "Bimestral",
+  "Trimestral",
+  "Cuatrimestral",
+  "Semestral",
+  "Anual",
+];
+
+const positiveValidation = value => {
+  const parsed = Number(value);
+  if (!Number.isNaN(parsed) && parsed > 0) {
+    return true;
+  }
+  return "Ingrese un valor positivo";
+};
+
+const loanTimeValidation = value => {
+  const parsed = Number(value);
+  if (!Number.isNaN(parsed) && parsed >= 2) {
+    return true;
+  }
+  return "Ingrese un valor mayor a 2";
+};
+
+const percentageValidation = value => {
+  const parsed = Number(value);
+  if (!Number.isNaN(parsed) && parsed > 0 && parsed < 100) {
+    return true;
+  }
+  return "Ingrese un valor porcentual adecuado";
+};
+
+const extraCostValidation = (value, extraCosts) => {
+  if (extraCosts.valueType === "Monetario") {
+    return positiveValidation(value);
+  }
+  return percentageValidation(value);
+};
+
+const IGV = 0.18;
+// const IR = 0.3;
+
 const main = async () => {
   const inputs = await inquirer.prompt([
     {
-      name: "username",
+      name: "sellingPrice",
       type: "input",
-      message: "What's your name?",
-      default: "stranger",
-      validate: input => typeof input === "string",
+      message: "Especifique el precio de venta del activo: ",
+      validate: positiveValidation,
+      filter: value => {
+        const parsed = Number(value);
+        if (positiveValidation(parsed) === true) {
+          return parsed;
+        }
+        return value;
+      },
+      transformer: input => currencyFormatter.format(input),
     },
+    {
+      name: "paymentFrequency",
+      type: "list",
+      message: "Elija su frecuencia de pago: ",
+      choices: frequencies,
+      filter(paymentFrequency) {
+        return inDays(paymentFrequency);
+      },
+    },
+    {
+      name: "loanTime",
+      type: "input",
+      message: "Especifique el tiempo total de pago (mínimo 2): ",
+      validate: loanTimeValidation,
+      filter: value => {
+        const parsed = Number(value);
+        if (loanTimeValidation(parsed) === true) {
+          return parsed;
+        }
+        return value;
+      },
+      transformer: input => `${input} años`,
+    },
+    {
+      name: "interestRateType",
+      type: "list",
+      message: "Elija el tipo de tasa de interés: ",
+      choices: ["Nominal", "Efectiva"],
+    },
+    {
+      name: "capitalizacion",
+      type: "list",
+      message: "Especifique la capitalizacion de la tasa nominal: ",
+      choices: frequencies,
+      when: answers => answers.interestRateType === "Nominal",
+      filter: capitalizacion => inDays(capitalizacion),
+    },
+    {
+      name: "interestRateFrequency",
+      type: "list",
+      message: "Especifique la frecuencia de la tasa de interés: ",
+      choices: frequencies,
+      filter(interestRateFrequency) {
+        return inDays(interestRateFrequency);
+      },
+    },
+    {
+      name: "interestRate",
+      type: "input",
+      message: "Ingrese el valor de la tasa de interés: ",
+      validate: percentageValidation,
+      filter: value => {
+        const parsed = Number(value);
+        if (percentageValidation(parsed) === true) {
+          return parsed;
+        }
+        return value;
+      },
+      transformer: input => `${input}%`,
+    },
+    {
+      name: "extraCosts",
+      type: "confirm",
+      message: "¿Desea registrar costos extra (iniciales o periódicos)?",
+      default: true,
+    },
+    /*
+    {
+      name: "periodicCommission",
+      type: "input",
+      message: "Ingrese el monto de comisión periódica: ",
+      validate: positiveValidation,
+      filter: value => {
+        const parsed = Number(value);
+        if (positiveValidation(parsed) === true) {
+          return parsed;
+        }
+        return value;
+      },
+      transformer: input => currencyFormatter.format(input),
+    },
+    {
+      name: "insurance",
+      type: "input",
+      message: "Ingrese el porcentaje del seguro contra todo riesgo: ",
+      validate: percentageValidation,
+      filter: value => {
+        const parsed = Number(value);
+        if (percentageValidation(parsed) === true) {
+          return parsed;
+        }
+        return value;
+      },
+      transformer: input => `${input}%`,
+    }, */
   ]);
 
-  console.log(`Hello, ${inputs.username}!`);
+  const extraCosts = [];
+
+  if (inputs.extraCosts) {
+    do {
+      const data = await inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "Nombre del costo: ",
+          validate: input => input.length > 0,
+        },
+        {
+          name: "type",
+          type: "list",
+          message: "Elija el tipo de gasto: ",
+          choices: ["Inicial", "Periódico"],
+        },
+        {
+          name: "valueType",
+          type: "list",
+          message: "Indique el tipo de valor: ",
+          choices: ["Monetario", "Porcentual"],
+        },
+        {
+          name: "value",
+          type: "input",
+          message: "Ingrese el valor: ",
+          validate: extraCostValidation,
+          transformer: (value, answers) => {
+            if (answers.valueType === "Monetario") {
+              return currencyFormatter.format(value);
+            }
+            return percentageFormatter.format(value / 100);
+          },
+        },
+        {
+          name: "keepAdding",
+          type: "confirm",
+          message: "¿Quiere continuar añadiendo costos?",
+          default: true,
+        },
+      ]);
+      extraCosts.push(data);
+    } while (extraCosts[extraCosts.length - 1].keepAdding);
+  }
+
+  const loanTimeInDays = inputs.loanTime * 360;
+  const montoIGV = roundMoney(inputs.sellingPrice * IGV);
+  const valorVenta = roundMoney(inputs.sellingPrice - montoIGV);
+
+  const cuotasAnuales = 360 / inputs.paymentFrequency;
+  const periodos = loanTimeInDays / inputs.paymentFrequency;
+
+  let periodicalInterestRate = 0;
+
+  if (inputs.interestRateType === "Nominal") {
+    const m = inputs.interestRateFrequency / inputs.capitalizacion;
+    const n = inputs.paymentFrequency / inputs.capitalizacion;
+    periodicalInterestRate = Math.pow(1 + inputs.interestRate / 100 / m, n) - 1;
+  } else {
+    periodicalInterestRate =
+      Math.pow(1 + inputs.interestRate / 100, inputs.paymentFrequency / inputs.interestRateFrequency) - 1;
+  }
+
+  console.log(`Precio de venta del activo: S/${inputs.sellingPrice}`);
+  console.log(`Monto de IGV: S/${montoIGV}`);
+  console.log(`Valor de venta del activo: S/${valorVenta}`);
+
+  console.log(`N° cuotas al año: ${cuotasAnuales}`);
+  console.log(`N° periodos de pago: ${periodos}`);
+  console.log(`Frecuencia de pago: ${inputs.paymentFrequency} días`);
+
+  console.log(`Tipo de tasa de interés: ${inputs.interestRateType}`);
+  console.log(`Tasa efectiva del periodo: ${periodicalInterestRate * 100}%`);
+  for (const extraCost of extraCosts) {
+    console.log(`Nombre del gasto extra: ${extraCost.name}`);
+    console.log(`Tipo: ${extraCost.type}`);
+    console.log(`Tipo de valor: ${extraCost.valueType}`);
+    console.log(`Valor: ${extraCost.value}`);
+  }
 };
 
 main();
