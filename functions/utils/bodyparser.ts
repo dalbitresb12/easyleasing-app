d9c11c07334ba700ebad157b6300b2e2da84e7ff
Diego Albitres <diego.albitres@gmail.com>
2022-11-19 11:11:29 -0500

    feat(api): added body parser abstraction

---
+++ functions/utils/bodyparser.ts
@@ -0,0 +1,14 @@
+import { z, ZodObject, ZodRawShape } from "zod";
+import { ValidationError } from "../types/httperror";
+
+export const parseBody = async <R extends ZodRawShape, T extends ZodObject<R>>(
+  request: Request,
+  model: T,
+): Promise<z.infer<T>> => {
+  const body = await request.json();
+  const parsed = model.safeParse(body);
+  if (!parsed.success) {
+    throw new ValidationError(parsed.error);
+  }
+  return parsed.data;
+};
