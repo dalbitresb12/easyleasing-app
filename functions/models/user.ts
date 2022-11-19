6484910b90829df32cf29c7fb73adfeddad84d5d
Diego Albitres <diego.albitres@gmail.com>
2022-11-19 13:07:41 -0500

    feat(users): add preferences to user model

--- functions/models/user.ts
+++ functions/models/user.ts
@@ -3,11 +3,15 @@ import { z } from "zod";
 export const User = z.object({
   uuid: z.string().uuid(),
   fullName: z.string().min(1),
-  fullname: z.string().min(1),
+  preferredName: z.string().min(1),
   email: z.string().email(),
   password: z.string().min(8).max(72),
   verified: z.boolean().default(false).optional(),
   verificationCode: z.string().optional(),
+  currency: z.string().length(3).default("PEN").optional(),
+  interest_rate_type: z.enum(["nominal", "effective"]).default("nominal").optional(),
+  language: z.string().length(2).default("es").optional(),
+  timezone: z.string().default("America/Lima").optional(),
 });
 
 export type User = z.infer<typeof User>;
