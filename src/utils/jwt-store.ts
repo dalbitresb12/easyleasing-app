539a877c79508717175c98c79e72cb5eec7593d4
Diego Albitres <diego.albitres@gmail.com>
2022-11-20 02:03:04 -0500

    feat(jwt): initial implementation of JwtStore

---
+++ src/utils/jwt-store.ts
@@ -0,0 +1,28 @@
+export class JwtStore {
+  static readonly key = "JWT_TOKEN";
+
+  static save(token: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      try {
+        localStorage.setItem(this.key, token);
+        resolve(true);
+      } catch (err) {
+        reject(false);
+      }
+    });
+  }
+
+  static get(): Promise<string | null> {
+    return new Promise(resolve => {
+      const token = localStorage.getItem(this.key);
+      resolve(token);
+    });
+  }
+
+  static clear(): Promise<boolean> {
+    return new Promise(resolve => {
+      localStorage.removeItem(this.key);
+      resolve(true);
+    });
+  }
+}
