a0e544e9e3c849e56d79e9db0fbe31a739ccc950
Diego Albitres <diego.albitres@gmail.com>
2022-11-21 23:06:26 -0500

    feat(api): add createdAt and updatedAt props to User

--- shared/models/user.ts
+++ shared/models/user.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
 
+export const DateWithParsing = z.preprocess(arg => {
+  if (typeof arg === "string" || arg instanceof Date) return new Date(arg);
+}, z.date());
+export type DateWithParsing = z.infer<typeof DateWithParsing>;
+
 export const User = z.object({
   uuid: z.string().uuid(),
   fullName: z.string().min(1, "Requerido"),
@@ -9,14 +14,16 @@ export const User = z.object({
     .string()
     .min(8, "La contraseña debe tener mínimo 8 caracteres")
     .max(72, "La contraseña debe tener máximo 72 caracteres"),
+  createdAt: DateWithParsing,
+  updatedAt: DateWithParsing,
   profilePicture: z.string().uuid().optional(),
-  verified: z.boolean().default(false).optional(),
   verificationCode: z.string().optional(),
-  currency: z.string().length(3, "Debe ser un código ISO 4217 válido").default("PEN").optional(),
-  interest_rate_type: z.enum(["nominal", "effective"]).default("nominal").optional(),
-  language: z.string().length(2, "Debe ser un código ISO 639-1 válido").default("es").optional(),
-  timezone: z.string().default("America/Lima").optional(),
-  dateFormat: z.string().default("DD/MM/YYYY").optional(),
+  verified: z.boolean().default(false),
+  currency: z.string().length(3, "Debe ser un código ISO 4217 válido").default("PEN"),
+  interest_rate_type: z.enum(["nominal", "effective"]).default("nominal"),
+  language: z.string().length(2, "Debe ser un código ISO 639-1 válido").default("es"),
+  timezone: z.string().default("America/Lima"),
+  dateFormat: z.string().default("DD/MM/YYYY"),
 });
 export type User = z.infer<typeof User>;
 
