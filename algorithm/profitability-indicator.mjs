c6c9caf5f2360f3cddc43806c6a9af38dfc20c03
Angel Meneses <angelmenesest123@gmail.com>
2022-11-23 02:34:35 -0500

    feat: vna calculation algorithm

--- algorithm/profitability-indicator.mjs
+++ algorithm/profitability-indicator.mjs
@@ -1,5 +1,15 @@
 import { irr } from "financial";
 
+export const vnaCalculation = (cashFlows, rate) => {
+  let vna = 0;
+
+  for (let iterator = 0; iterator < cashFlows.length; iterator++) {
+    vna += cashFlows[iterator] / Math.pow(1 + rate, iterator);
+  }
+
+  return vna;
+};
+
 export const tirCalculation = cashFlows => {
   return irr(cashFlows, 0.01);
 };
